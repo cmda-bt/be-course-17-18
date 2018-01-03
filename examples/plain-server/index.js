c25672a85c8e88230ad8dc57c13c7f5a3cbbaa67
Titus Wormer <tituswormer@gmail.com>
2018-01-03 21:22:48 +0100

    examples:plain-server: add about route

--- examples/plain-server/index.js
+++ examples/plain-server/index.js
@@ -7,5 +7,9 @@ http.createServer(onrequest).listen(8000)
 function onrequest(req, res) {
   res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
-  res.end('Hello World\n')
+  if (req.url === '/about') {
+    res.end('This is my website\n')
+  } else {
+    res.end('Hello World\n')
+  }
 }
