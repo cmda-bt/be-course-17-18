5befbbc3f06b02aa921e9d61f794a73777f5cadd
Titus Wormer <tituswormer@gmail.com>
2018-01-03 21:25:01 +0100

    examples:plain-server: refactor routes

--- examples/plain-server/index.js
+++ examples/plain-server/index.js
@@ -2,16 +2,18 @@
 
 var http = require('http')
 
+var routes = {
+  '/about': 'This is my website\n',
+  '/': 'Hello World\n'
+}
+
 http.createServer(onrequest).listen(8000)
 
 function onrequest(req, res) {
   res.setHeader('Content-Type', 'text/html')
-  if (req.url === '/about') {
+  if (req.url in routes) {
     res.statusCode = 200
-    res.end('This is my website\n')
-  } else if (req.url === '/') {
-    res.statusCode = 200
-    res.end('Hello World\n')
+    res.end(routes[req.url])
   } else {
     res.statusCode = 404
     res.end('Not found\n')
