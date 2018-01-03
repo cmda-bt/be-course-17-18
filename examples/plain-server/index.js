6fa184f07dcff5f031adb663abaf2248ff595c52
Titus Wormer <tituswormer@gmail.com>
2018-01-03 21:24:06 +0100

    examples:plain-server: add initial support for unfound routes

--- examples/plain-server/index.js
+++ examples/plain-server/index.js
@@ -5,11 +5,15 @@ var http = require('http')
 http.createServer(onrequest).listen(8000)
 
 function onrequest(req, res) {
-  res.statusCode = 200
   res.setHeader('Content-Type', 'text/html')
   if (req.url === '/about') {
+    res.statusCode = 200
     res.end('This is my website\n')
-  } else {
+  } else if (req.url === '/') {
+    res.statusCode = 200
     res.end('Hello World\n')
+  } else {
+    res.statusCode = 404
+    res.end('Not found\n')
   }
 }
