ad6ed8a3ac5ca1dea943bce3ee3325632e87f2ab
Titus Wormer <tituswormer@gmail.com>
2018-02-28 19:16:23 +0100

    examples:mysql-server: remove superfluous data

--- examples/mysql-server/index.js
+++ examples/mysql-server/index.js
@@ -18,21 +18,6 @@ var connection = mysql.createConnection({
 
 connection.connect()
 
-var data = [
-  {
-    id: 'evil-dead',
-    title: 'Evil Dead',
-    plot: 'Five friends travel to a cabin in the woods, where they unknowingly release flesh-possessing demons.',
-    description: 'Five friends head to a remote cabin, where the discovery of a Book of the Dead leads them to unwittingly summon up demons living in the nearby woods. The evil presence possesses them until only one is left to fight for survival.'
-  },
-  {
-    id: 'the-shawshank-redemption',
-    title: 'The Shawshank Redemption',
-    plot: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-    description: 'Andy Dufresne is a young and successful banker whose life changes drastically when he is convicted and sentenced to life imprisonment for the murder of his wife and her lover. Set in the 1940’s, the film shows how Andy, with the help of his friend Red, the prison entrepreneur, turns out to be a most unconventional prisoner.'
-  }
-]
-
 var upload = multer({dest: 'static/upload/'})
 
 express()
